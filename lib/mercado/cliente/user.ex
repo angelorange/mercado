25821095d0086006469261eb3704a05758a6a4b7
angelorange <acostalarangeira@gmail.com>
2021-03-03 11:00:25 -0300

    feat: add some things to user migration and model

--- lib/mercado/cliente/user.ex
+++ lib/mercado/cliente/user.ex
@@ -4,14 +4,17 @@ defmodule Mercado.Cliente.User do
 
   schema "users" do
     field :email, :string
+    field :sexo, :string
+    field :nome, :string
 
     timestamps()
   end
 
+  @required_params [:email, :sexo, :nome]
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email])
-    |> validate_required([:email])
+    |> cast(attrs, @required_params)
+    |> validate_required(@required_params)
   end
 end
--- priv/repo/migrations/20210302212046_create_users.exs
+++ priv/repo/migrations/20210302212046_create_users.exs
@@ -4,9 +4,10 @@ defmodule Mercado.Repo.Migrations.CreateUsers do
   def change do
     create table(:users) do
       add :email, :string
+      add :sexo, :string
+      add :nome, :string
 
       timestamps()
     end
-
   end
 end
